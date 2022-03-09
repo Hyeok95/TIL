455538d0403f4b5f2483b446a68aa13a46e7b94e
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-03-09 23:38:29 +0900

    Create 백준1316.py

---
+++ Python/Python-Algorithms/BaekJoon/문자열/백준1316.py
@@ -0,0 +1,11 @@
+n = int(input())
+result = n
+for i in range(n):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            result -=1
+            break
+print(result)
