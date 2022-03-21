2b6b306c57be2a8a0c219f86ffad2ccfef237553
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-03-21 10:07:14 +0900

    Create 백준1003.py

---
+++ Python/Python-Algorithms/BaekJoon/백준1003.py
@@ -0,0 +1,16 @@
+#백준 1003
+n = int(input())
+
+zero = [1,0,1]
+one = [0,1,1]
+
+def fibo(num):
+  length  = len(zero)
+  if length <= num:
+    for i in range(length, num+1):
+      zero.append(zero[i-1]+ zero[i-2])
+      one.append(one[i-1] + one[i-2])
+  print("%d %d"%(zero[num],one[num]))
+
+for i in range(n):
+  fibo(int(input()))
