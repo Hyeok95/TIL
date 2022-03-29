e8d4582e8d4ee8c84c9c98ed00cccb7b3c0a6aea
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-03-29 11:15:56 +0900

    Update 백준2869.py

--- Python/Python-Algorithms/BaekJoon/수학/백준2869.py
+++ Python/Python-Algorithms/BaekJoon/수학/백준2869.py
@@ -1,13 +1,20 @@
 a,b,v = map(int, input().split())
 
-count = 0
-height = 0
-while True:
-  height += a
-  count += 1
+# 시간초과 코드
+# count = 0
+# height = 0
+# while True:
+#   height += a
+#   count += 1
 
-  if height == v:
-    break
-  height -=b
+#   if height == v:
+#     break
+#   height -=b
   
-print(count)
+# print(count)
+
+import math
+
+a,b,v = map(int, input().split())
+day = (v-b) /(a-b)
+print(math.ceil(day))
