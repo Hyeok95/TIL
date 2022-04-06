1378c5e8f6aa9652f3c6f56049b005ab2e29e6b8
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-04-06 11:00:31 +0900

    Create 백준1927.py

---
+++ Python/Python-Algorithms/BaekJoon/우선순위 큐/백준1927.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+heap = []
+for i in range(n):
+  num = int(input())
+  if num == 0:
+    if heap:
+      print(heapq.heappop(heap))
+    else:
+      print(0)
+  else:
+    heapq.heappush(heap, num)
