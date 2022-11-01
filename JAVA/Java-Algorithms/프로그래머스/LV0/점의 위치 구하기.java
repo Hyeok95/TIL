6771cbb74138453ff4cd902ad69ca6c856e33328
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-11-01 09:01:49 +0900

    Create 점의 위치 구하기.java

---
+++ JAVA/Java-Algorithms/프로그래머스/LV0/점의 위치 구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        if (dot[0] > 0 && dot[1] > 0){
+            return 1;
+        } else if (dot[0] < 0 && dot[1] > 0) {
+            return 2;
+        } else if (dot[0] < 0 && dot[1] < 0){
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
