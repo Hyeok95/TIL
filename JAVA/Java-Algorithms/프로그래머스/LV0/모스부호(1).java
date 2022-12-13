32b75194a5e0fbed2db7e5c37f0dda6d1cf1cfaf
ChangHyeok Ahn <78522684+Hyeok95@users.noreply.github.com>
2022-12-13 09:01:18 +0900

    Create 모스부호(1).java

---
+++ JAVA/Java-Algorithms/프로그래머스/LV0/모스부호(1).java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        Map<String, String> alphabetMap = new HashMap<>() {
+            {
+                put(".-", "a");
+                put("-...", "b");
+                put("-.-.", "c");
+                put("-..", "d");
+                put(".", "e");
+                put("..-.", "f");
+                put("--.", "g");
+                put("....", "h");
+                put("..", "i");
+                put(".---", "j");
+                put("-.-", "k");
+                put(".-..", "l");
+                put("--", "m");
+                put("-.", "n");
+                put("---", "o");
+                put(".--.", "p");
+                put("--.-", "q");
+                put(".-.", "r");
+                put("...", "s");
+                put("-", "t");
+                put("..-", "u");
+                put("...-", "v");
+                put(".--", "w");
+                put("-..-", "x");
+                put("-.--", "y");
+                put("--..", "z");
+            }
+        };
+        
+        String[] letterArr = letter.split(" ");
+        for (String arr : letterArr) {
+            answer += alphabetMap.get(arr);
+        }
+        
+        
+        return answer;
+    }
+}
